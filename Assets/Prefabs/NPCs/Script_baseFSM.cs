b5baf7088ddbd642ac66ca56080ed478a01514cd
Avarice0 <88842277+Avora-bit@users.noreply.github.com>
2023-02-16 17:20:16 +0800

    Prep FSM manager, rearrange testmap

--- Assets/Prefabs/NPCs/Script_baseFSM.cs
+++ Assets/Prefabs/NPCs/Script_baseFSM.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Script_baseFSM : MonoBehaviour
 {
+    FSM currentFSM = 0;
+    int iFSMCounter = 0;
+    public const float iMaxFSMCounter = 60f;       //in seconds
+    //after 1 min ends, stop chase and wait for other enemies to spawn and reset self FSM
+
     enum FSM
     {
+        INACTIVE = 0,
         IDLE,               //check for player
         PATROL,             //"melee": roam spawn point
         MOVE,               //found target, moving to target
         ATTACK,             //target in range, attack
         VANTAGE,            //"ranged": keep target in range, go to higher point, 
+        DEATH
     }
 
     // Start is called before the first frame update
@@ -22,6 +29,47 @@ public class Script_baseFSM : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch(currentFSM){
+            case FSM.INACTIVE:
+                {
+                    break;
+                }
+            case FSM.IDLE:
+                {
+                    break;
+                }
+            case FSM.PATROL:
+                {
+                    break;
+                }
+            case FSM.MOVE:
+                {
+                    break;
+                }
+            case FSM.ATTACK:
+                {
+                    break;
+                }
+            case FSM.VANTAGE:
+                {
+                    break;
+                }
+            case FSM.DEATH:
+                {
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        };
+
+
+    }
+
+    public void resetFSM()
+    {
+        currentFSM = FSM.INACTIVE;
+        iFSMCounter = 0;
     }
 }
