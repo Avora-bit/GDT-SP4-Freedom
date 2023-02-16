5d894df486f3c437f7bd224ec6a833524ca22737
blaster8524 <88960191+blaster8524@users.noreply.github.com>
2023-02-16 15:51:46 +0800

    Teleport scripts

--- Assets/Scripts/Script_Teleport.cs
+++ Assets/Scripts/Script_Teleport.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Script_Teleport : MonoBehaviour
 {
     public GameObject player;
+    public Vector3 HubTeleportPos = new Vector3(-10,(float)1.5,0);
+    public Vector3[] ArenaVectors;
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Player Entered TP area");
-            player.transform.position = new Vector3(-10, (float)1.5,0);
+            player.transform.position = HubTeleportPos;
         }
     }
 
