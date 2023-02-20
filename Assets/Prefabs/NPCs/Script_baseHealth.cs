50c6b4f69fe8c1541962215284c12b1e4d7de009
Avarice0 <88842277+Avora-bit@users.noreply.github.com>
2023-02-20 10:18:00 +0800

    Damage Text

--- Assets/Prefabs/NPCs/Script_baseHealth.cs
+++ Assets/Prefabs/NPCs/Script_baseHealth.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
+using TMPro;
+
 public class Script_baseHealth : MonoBehaviour
 {
     public int maxHealth = 100;
@@ -10,7 +13,9 @@ public class Script_baseHealth : MonoBehaviour
     public int totaldmg;
     public bool IsPlayer = false;
     public bool IsDummy = false;
-    
+
+    public GameObject damageTextPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,20 +37,40 @@ public class Script_baseHealth : MonoBehaviour
 
     public void TakeDamage(int _damage)
     {
+        string text = _damage + "!";
+
         selfHealth -= _damage;
         if (IsPlayer)
         {
             totaldmg += _damage;
         }
+        else FloatingText(text, Color.red);
     }
 
     public void Healing(int _heal)
     {
+        string text = ">" + _heal + "<"; 
         selfHealth = Mathf.Clamp(selfHealth += _heal, 0, maxHealth);
+        if (!IsPlayer) FloatingText(text, Color.green);
     }
 
     public int getHealth()
     {
         return selfHealth;
+    }
+
+
+    //if game object is dummy or npc, render damage text
+
+    public void FloatingText(string _text, Color _color)
+    {
+        GameObject DamageTextInstance = Instantiate(damageTextPrefab, gameObject.transform);
+        DamageTextInstance.transform.GetChild(0).GetComponent<TextMeshPro>().transform.rotation = Camera.main.transform.rotation;
+        DamageTextInstance.transform.GetChild(0).GetComponent<TextMeshPro>().SetText(_text);
+        DamageTextInstance.transform.GetChild(0).GetComponent<TextMeshPro>().color = _color;
+        if (selfHealth <= 0)
+        {
+
+        }
     }
 }
