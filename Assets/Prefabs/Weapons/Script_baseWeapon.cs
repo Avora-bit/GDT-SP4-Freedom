ff5385a0060d4d723a457fcf4a16f097955e23f3
Unknownfailure <lukeatan47@gmail.com>
2023-02-25 13:40:11 +0800

    buffed player, nerfed enemies, added items to the arenas

--- Assets/Prefabs/Weapons/Script_baseWeapon.cs
+++ Assets/Prefabs/Weapons/Script_baseWeapon.cs
@@ -113,7 +113,7 @@ public class Script_baseWeapon : MonoBehaviour
                     other.GetComponent<Script_baseHealth>().TakeDamage(iDamage / 2, gameObject);
                     if (other.GetComponent<Script_baseHealth>().InvincTimer <= 0.0f)
                     {
-                        other.GetComponent<Script_baseHealth>().InvincTimer = timeBetweenAttack;
+                        other.GetComponent<Script_baseHealth>().InvincTimer = timeBetweenAttack * 1.1f;
                     }
                 }
             }
